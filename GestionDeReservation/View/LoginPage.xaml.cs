014fb87c42614491d54cf93f5077f5ceb30f7d9f
Jeremy Lambrecq <Jeremy.lmbq@hotmail.com>
2024-04-14 11:50:14 +0200

    LoginPage => Connection button

--- GestionDeReservation/View/LoginPage.xaml.cs
+++ GestionDeReservation/View/LoginPage.xaml.cs
@@ -7,13 +7,20 @@ public partial class LoginPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
-
+        if (pseudo.Text == "jeremy" && pass.Text == "1234")
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+        else
+        {
+            await DisplayAlert("Erreur", "Pseudo ou mot de passe incorrect", "OK");
+        }
     }
 
-    private void Button_Clicked_1(object sender, EventArgs e)
+    private async void Button_Clicked_1(object sender, EventArgs e)
     {
-
+        await DisplayAlert("Erreur", "Pas encore implémenté", "OK");
     }
 }
